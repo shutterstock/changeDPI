4bcbfbe029b5fa174c136efbe635a1e198888481
Andrea Bogazzi <andreabogazzi79@gmail.com>
2018-08-11 09:08:11 +0200

    do not change dist at fix level

--- dist/index.js
+++ dist/index.js
@@ -72,7 +72,7 @@ function changeDpiDataUrl(base64Image, dpi) {
     type = PNG;
     var b64Index = detectPhysChunkFromDataUrl(body);
     // 28 bytes in dataUrl are 21bytes, length of phys chunk with everything inside.
-    if (b64Index >= 0) {
+    if (b64Index) {
       headerLength = Math.ceil((b64Index + 28) / 3) * 4;
       overwritepHYs = true;
     } else {
